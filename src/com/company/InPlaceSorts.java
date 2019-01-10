f546ff641ad4f4b9a526cbe1bcd958cb9c3a8cc5
BT_1N3_13 <a>
2019-01-10 08:37:54 -0500

    a

--- src/com/company/InPlaceSorts.java
+++ src/com/company/InPlaceSorts.java
@@ -2,7 +2,6 @@ package com.company;
 
 public class InPlaceSorts {
 
-
     // goes through the list, swaps while # is bigger than the next.
     public static void insertionSort(int[] list1){
         int temp;
@@ -19,6 +18,27 @@ public class InPlaceSorts {
             }
             b++;
             i = b+1;
+        }
+    }
+
+    //fix this to account for double int...?
+    public static void selectionSort(double[] list1){
+        int i; //goes through the whole list
+        int x = 0; //keeps track of the next smallest int
+        int y = 0; //keeps track of where to start next
+        double temp; //used to swap
+        while (y < list1.length - 1){
+            for (i = y ; i < list1.length; i++){
+                if (list1[x] > list1[i]){
+                    x = i;
+                }
+            }
+            temp = list1[y];
+            list1[y] = list1[x];
+            list1[x] = temp;
+            y++;
+            x = y;
+
         }
     }
 }
