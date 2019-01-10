34f4584724cfd569a1dea8eec2dc0cfa183811fd
BT_1N3_13 <a>
2019-01-10 08:32:53 -0500

    a

--- src/com/company/InPlaceSorts.java
+++ src/com/company/InPlaceSorts.java
@@ -2,21 +2,6 @@ package com.company;
 
 public class InPlaceSorts {
 
-    public static void insertionSort1(int[] list1){
-        // i goes through list once
-        // a goes through list several times and resets to find place to insert i
-        int b = 0; // b is index of where it will be inserted
-        for (int i = 0; i < list1.length ; i++ ){
-            for (int a = i+1; a < list1.length; a++ ){
-                if (list1[a] < list1[i]){
-                    b = a;
-                    //fix this
-                }
-            }
-            //updates the entire array. insert i into the array and update the rest
-            list1[i]
-        }
-    }
 
     // goes through the list, swaps while # is bigger than the next. resets at next starting int if it doesnt swap
 
@@ -25,17 +10,17 @@ public class InPlaceSorts {
         int temp;
         int i = 1;
         int b = 0; //next starting int
-        while (b < list1.length){
+        while (b < list1.length-1){
             while (i < list1.length){
-                if (list1[i-1] > list1[i]){
-                    temp = list1[i-1];
-                    list1[i-1] = list1[i];
+                if (list1[b] > list1[i]){
+                    temp = list1[b];
+                    list1[b] = list1[i];
                     list1[i] = temp;
-                    i++;
                 }
+                i++;
             }
-            i = b;
             b++;
+            i = b+1;
         }
     }
 }
--- src/com/company/Runner.java
+++ src/com/company/Runner.java
@@ -3,6 +3,19 @@ package com.company;
 public class Runner {
 
     public static void main(String[] args) {
-	// write your code here
+        int[] testArr = {8,6,7,5,3,0,9,10,1,2,3};
+        System.out.print("Before: ");
+        for (int num:testArr)
+            System.out.print(num+" ");
+        System.out.println();
+
+        InPlaceSorts.insertionSort(testArr);
+
+        //Print it out after you sort
+        System.out.print("After: ");
+        for (int num:testArr)
+            System.out.print(num+" ");
+        System.out.println();
+
     }
 }
